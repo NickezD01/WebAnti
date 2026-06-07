2bdc3722ec1d6fb4ae536f69a72a4c86d76971c4
Nick <vinhngalong123@gmail.com>
2026-06-08 01:22:17 +0700

    .

--- AntiPhisher/AntiPhisher.Domain/Models/Order.cs
+++ AntiPhisher/AntiPhisher.Domain/Models/Order.cs
@@ -14,11 +14,18 @@ namespace AntiPhisher.Domain.Models
         public int SubscriptionId { get; set; }
         public decimal? Price { get; set; }
         public string Note { get; set; }
-        //public OrderStatus Status { get; set; } = OrderStatus.Pending; // Pending, Paid, Canceled
+        public OrderStatus Status { get; set; } = OrderStatus.Pending; // Pending, Paid, Canceled
         public bool IsDelete { get; set; }
         public User? UserAccount { get; set; }
         public Subscription Subscription { get; set; }
         //public SubscriptionPlan? SubscriptionPlans { get; set; }
         public List<Payment>? Payments { get; set; }
     }
+    public enum OrderStatus
+    {
+        Pending,
+        Paid,
+        Canceled
+    }
+
 }
